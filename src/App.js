1f8d6d0963108c91a9323b47406b123ad58c5f15
Siddhesh Panhale <65393309+sidpanhale@users.noreply.github.com>
2021-07-23 11:46:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import SimpleInput from './components/SimpleInput'
+import BasicForm from './components/BasicForm'
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <SimpleInput />
+      <BasicForm />
     </div>
   );
 }
